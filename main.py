fc37830fe7839a47b011093513b5e9aefca9b096
Amelia Fitzsimmons <113189695+AmeliaFitzsimmons@users.noreply.github.com>
2022-12-07 15:18:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+answer1 = input("What is the first colour in the rainbow? \n a. Orange \n b. Yellow \n c. Red \n d. Blue \nAnswer: ")
+if answer1 == "c" or answer1 == "Red":
+  print("Correct!")
+else:
+  print("Incorrect! The answer is Red.")
